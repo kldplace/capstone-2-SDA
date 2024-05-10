3187b81536e99668d4df61a1bd9d6b29159b804b
kldplace <kldplace>
2024-05-10 17:16:46 +0000

    set up my environment variables/mount Blob to DBFS

--- Databricks-workspace/Mount_Storage_container.py
+++ Databricks-workspace/Mount_Storage_container.py
@@ -1,28 +1,33 @@
 # Databricks notebook source
-!pip install python-dotenv
-
-
-# COMMAND ----------
-
-dbutils.library.restartPython()
+# Import [dotenv] package
+%pip install python-dotenv
 
 # COMMAND ----------
 
-# MAGIC %load_ext dotenv
-# MAGIC %dotenv
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# My environment variables
+storage_account_access_key = os.environ.get('STORAGE_ACCOUNT_ACCESS_KEY')
+storage_account_name = os.environ.get('STORAGE_ACCOUNT_NAME')
+container_name = os.environ.get('CONTAINER_NAME')
 
 # COMMAND ----------
 
-# Define the storage account access key
-storage_account_access_key = ""
-
-# Set up the configuration with your storage account name and access key
+# Set up the configuration with the storage account name and access key
 configs = {
-  "fs.azure.account.key.capstone2datalake.blob.core.windows.net": storage_account_access_key
+  f"fs.azure.account.key.{storage_account_name}.blob.core.windows.net": storage_account_access_key
 }
 
 # Mount the Blob Storage container to DBFS
 dbutils.fs.mount(
-  source = "wasbs://capstone-2@capstone2datalake.blob.core.windows.net/",
+  source = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net/",
   mount_point = "/mnt/capstoneFolders",
   extra_configs = configs)
+
+# COMMAND ----------
+
+# Use this to unmount the Blob Storage container to DBFS
+#dbutils.fs.unmount("/mnt/capstoneFolders")
